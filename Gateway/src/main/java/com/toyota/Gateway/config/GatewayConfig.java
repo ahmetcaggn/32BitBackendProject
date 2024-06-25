f4097cd4e99bd827fc6726ac00124a008454839b
ahmetcaggn <ahmetcannersoy@gmail.com>
2024-06-25 18:42:13 +0300

    feat: Retry mechanisim added to Route Locator because of 503 Service Unavailable Error

--- Gateway/src/main/java/com/toyota/Gateway/config/GatewayConfig.java
+++ Gateway/src/main/java/com/toyota/Gateway/config/GatewayConfig.java
@@ -5,12 +5,14 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class GatewayConfig {
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
@@ -21,31 +23,72 @@ public class GatewayConfig {
 
                 .route(r -> r
                         .path("/sales/**")
-                        .filters(f -> f.filter(authFilter.apply(new CustomAuthFilter.Config())))
+                        .filters(f -> f
+                                .filter(authFilter.apply(new CustomAuthFilter.Config()))
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(10)
+                                        .setStatuses(HttpStatus.SERVICE_UNAVAILABLE)
+                                        .setMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                                )
+                        )
+
                         .uri("lb://SALES")
                 )
-                .route(r-> r
+                .route(r -> r
                         .path("/campaigns/**")
-                        .filters(f -> f.filter(authFilter.apply(new CustomAuthFilter.Config())))
+                        .filters(f -> f
+                                .filter(authFilter.apply(new CustomAuthFilter.Config()))
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(10)
+                                        .setStatuses(HttpStatus.SERVICE_UNAVAILABLE)
+                                        .setMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                                ))
                         .uri("lb://SALES")
                 )
                 .route(r -> r
                         .path("/product/**")
-//                        .filters(f -> f.filter(authFilter.apply(new CustomAuthFilter.Config())))
+                        .filters(f -> f
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(10)
+                                        .setStatuses(HttpStatus.SERVICE_UNAVAILABLE)
+                                        .setMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                                )
+                        )
                         .uri("lb://PRODUCT")
                 )
-                .route(r-> r
+                .route(r -> r
                         .path("/employee/**")
-                        .filters(f -> f.filter(authFilter.apply(new CustomAuthFilter.Config())))
+                        .filters(f -> f
+                                .filter(authFilter.apply(new CustomAuthFilter.Config()))
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(10)
+                                        .setStatuses(HttpStatus.SERVICE_UNAVAILABLE)
+                                        .setMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                                )
+                        )
                         .uri("lb://USERMANAGEMENT")
                 )
-                .route(r-> r
+                .route(r -> r
                         .path("/report/**")
-                        .filters(f -> f.filter(authFilter.apply(new CustomAuthFilter.Config())))
+                        .filters(f -> f
+                                .filter(authFilter.apply(new CustomAuthFilter.Config()))
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(10)
+                                        .setStatuses(HttpStatus.SERVICE_UNAVAILABLE)
+                                        .setMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                                )
+                        )
                         .uri("lb://REPORT")
                 )
-                .route(r-> r
+                .route(r -> r
                         .path("/security/**")
+                        .filters(f -> f
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(10)
+                                        .setStatuses(HttpStatus.SERVICE_UNAVAILABLE)
+                                        .setMethods(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                                )
+                        )
                         .uri("lb://SECURITY")
                 )
                 .build();
